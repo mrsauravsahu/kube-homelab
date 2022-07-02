a1d0a81da3a90d6b12b92e60de6913c103c1fe82
Saurav Sahu <mrsauravsahu@outlook.com>
2022-07-02 14:45:17 +0530

    feat: create base tier with terraform

---
+++ iac/providers.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    helm = {
+      source = "hashicorp/helm"
+      version = "2.6.0"
+    }
+
+    k8s = {
+      source = "hashicorp/kubernetes"
+      version = "2.12.0"
+    }
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = var.cluster.config_path
+  }
+}
+
+provider "k8s" {
+  config_path = var.cluster.config_path
+  config_context = var.cluster.name
+}
+
