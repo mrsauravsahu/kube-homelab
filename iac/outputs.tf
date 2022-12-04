024eae492d5c062eb598a1c62317b9df41dcf45a
Saurav Sahu <mrsauravsahu@outlook.com>
2022-12-04 15:35:43 +0530

    feat: instal nginx ingress (#13)

---
+++ iac/outputs.tf
@@ -0,0 +1,11 @@
+output "base" {
+  description = "Base namespace of the homelab"
+  value = {
+    ns = var.base.name
+  }
+}
+
+output "apps" {
+  description = "Installed applications list"
+  value = [for app in helm_release.external_apps.* : app.name ]
+}
