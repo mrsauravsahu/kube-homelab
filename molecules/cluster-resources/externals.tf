6ecf1ccae98db646ee084096f4e7e51bed992367
Saurav Sahu <mrsauravsahu@outlook.com>
2022-12-24 17:21:57 +0530

    move iac molecules to molecules folder

--- molecules/cluster-resources/externals.tf
+++ molecules/cluster-resources/externals.tf
@@ -5,18 +5,18 @@ resource "helm_release" "external_apps" {
   chart      = var.externals[count.index].name
   namespace  = var.externals[count.index].namespace != null ? var.externals[count.index].namespace : var.externals[count.index].name
   create_namespace = true
-  values = fileexists("${path.module}/../config/externals/${var.externals[count.index].name}/values.yaml") == true ? ["${file("${path.module}/../config/externals/${var.externals[count.index].name}/values.yaml")}"] : []
+  values = fileexists("./config/externals/${var.externals[count.index].name}/values.yaml") == true ? ["${file("./config/externals/${var.externals[count.index].name}/values.yaml")}"] : []
 
   atomic     = true
 }
 
 resource "helm_release" "external_ingresses" {
-  for_each     = {for app in helm_release.external_apps.* : app.name => app if fileexists("${path.module}/../config/externals/${app.name}/ingress.values.yaml")}
+  for_each     = {for app in helm_release.external_apps.* : app.name => app if fileexists("./config/externals/${app.name}/ingress.values.yaml")}
   name       = "${each.value.name}-ingress"
-  chart      = "${path.module}/../lib/helm-chart-homelab-ingress"
+  chart      = "./lib/helm-chart-homelab-ingress"
   namespace  = each.value.namespace
 
-  values = ["${file("${path.module}/../config/externals/${each.value.name}/ingress.values.yaml")}"]
+  values = ["${file("./config/externals/${each.value.name}/ingress.values.yaml")}"]
 
   atomic     = true
   depends_on = [
