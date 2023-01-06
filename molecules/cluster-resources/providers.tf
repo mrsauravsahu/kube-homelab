3e2f5317f7575a284b408299b8aafce7dd2497cc
Saurav Sahu <mrsauravsahu@outlook.com>
2023-01-06 21:29:12 +0530

    chore: pin tf version to >=1.3.0

--- molecules/cluster-resources/providers.tf
+++ molecules/cluster-resources/providers.tf
@@ -1,4 +1,6 @@
 terraform {
+  required_version = ">=1.3.0"
+
   required_providers {
     helm = {
       source = "hashicorp/helm"
@@ -10,7 +12,7 @@ terraform {
       version = "2.12.0"
     }
   }
-
+  
   backend "http" { }
 }
 
