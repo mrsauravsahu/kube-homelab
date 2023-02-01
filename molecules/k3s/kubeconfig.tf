da524bf1d8eed1c2a48495ee96d0db5c1d74e043
Saurav Sahu <mrsauravsahu@outlook.com>
2023-02-01 23:16:02 +0530

    improv: simplify k3s creation (#37)

--- molecules/k3s/kubeconfig.tf
+++ molecules/k3s/kubeconfig.tf
@@ -3,8 +3,11 @@ data "remote_file" "kubeconfig" {
   conn {
     host        = each.value.host
     user        = each.value.user
-    private_key = file(var.host.private_key)
+    private_key = file(each.value.private_key)
   }
 
   path        = "/etc/rancher/k3s/k3s.yaml"
+  depends_on  = [
+    ssh_resource.install_k3s
+  ]
 }
--- molecules/k3s/server.tf
+++ molecules/k3s/server.tf
@@ -1,32 +1,23 @@
 resource "ssh_resource" "install_k3s" {
-  for_each   = local.servers
-  host            = each.value.host
-  user = each.value.user
-  host_user = var.host.user
-  commands            = [
+  for_each = local.servers
+  host     = each.value.host
+  user     = each.value.user
+  commands = [
     "curl -sfL ${local.k3s.download_url} | INSTALL_K3S_VERSION='${local.k3s.version}' sh -s - server --docker --write-kubeconfig-mode 644 --disable=traefik"
     # TODO: For HA k3s cluster setup
     # "curl -sfL ${local.k3s.download_url} | INSTALL_K3S_VERSION='${local.k3s.version}' sh -s - server --cluster-init --docker --write-kubeconfig-mode 644 --disable=traefik"
   ]
-  private_key        = file(var.host.private_key)
+  private_key = file(each.value.private_key)
+  timeout = "10m"
 }
 
-# TODO: replace with some form of wait until k3s is ready
-resource "time_sleep" "wait_k3s_to_get_ready" {
-  depends_on = [ssh_resource.install_k3s]
-  create_duration = "30s"
-}
+# Note: Removed waiting for k3s server to be ready
 
 resource "ssh_resource" "uninstall_k3s" {
-  for_each   = {for server in var.servers : server.host => server}
-  host            = each.value.host
-  when = "destroy"
-  user = each.value.user
-  host_user = var.host.user
-  commands            = [
-    "k3s-killall.sh",
-    "k3s-uninstall.sh",
-    "docker ps -q | xargs docker stop && docker system prune -f"
-    ]
-  private_key        = file(var.host.private_key)
+  for_each    = { for server in var.servers : server.host => server }
+  host        = each.value.host
+  when        = "destroy"
+  user        = each.value.user
+  commands    = ["bash -c 'k3s-killall.sh; k3s-uninstall.sh;'"]
+  private_key = file(each.value.private_key)
 }
