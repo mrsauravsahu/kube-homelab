6de05cb86d0e985ad2642dbc6c444f36245e42a0
Saurav Sahu <mrsauravsahu@outlook.com>
2023-02-10 01:15:17 +0530

    feat: install open telemetry collector

--- molecules/cluster-resources/inputs.tf
+++ molecules/cluster-resources/inputs.tf
@@ -11,38 +11,38 @@ variable "base" {
 variable "cluster" {
   description = "Details about the cluster"
   type = object({
-    name = string
+    name        = string
     config_path = string
-    host = string
-    hosts = list(string)
+    host        = string
+    hosts       = list(string)
   })
 }
 
 variable "externals" {
   description = "Details about external items to install"
   type = list(object({
-    name = string
-    repo = string
-    version = string
+    name      = string
+    repo      = string
+    version   = string
     namespace = optional(string)
   }))
   // Feel free to move the applications arround, the order in the array does not matter
   default = [
     {
-      name = "ingress-nginx"
-      repo = "https://kubernetes.github.io/ingress-nginx"
+      name    = "ingress-nginx"
+      repo    = "https://kubernetes.github.io/ingress-nginx"
       version = "4.4.0"
     },
     {
-      name = "hajimari"
-      repo = "https://hajimari.io"
-      version = "1.2.0"
+      name      = "hajimari"
+      repo      = "https://hajimari.io"
+      version   = "1.2.0"
       namespace = "homelab"
     },
     {
-      name = "postgresql"
-      repo = "https://cetic.github.io/helm-charts"
-      version = "0.2.3"
+      name      = "postgresql"
+      repo      = "https://cetic.github.io/helm-charts"
+      version   = "0.2.3"
       namespace = "homelab"
     },
     // TODO: nextcloud fails due to slow startup, need to figure this out
@@ -59,27 +59,33 @@ variable "externals" {
     #   namespace = "homelab"
     # }
     {
-      name = "pihole",
-      repo = "https://mojo2600.github.io/pihole-kubernetes/",
-      version = "2.11.0",
+      name      = "pihole",
+      repo      = "https://mojo2600.github.io/pihole-kubernetes/",
+      version   = "2.11.0",
       namespace = "homelab"
     },
     {
-      name = "keycloak",
-      repo = "https://codecentric.github.io/helm-charts",
-      version = "20.0.3",
+      name      = "keycloak",
+      repo      = "https://codecentric.github.io/helm-charts",
+      version   = "20.0.3",
       namespace = "homelab"
     },
     {
-      name = "prometheus",
-      repo = "https://prometheus-community.github.io/helm-charts",
-      version = "19.3.1",
+      name      = "prometheus",
+      repo      = "https://prometheus-community.github.io/helm-charts",
+      version   = "19.3.1",
       namespace = "homelab"
     },
     {
-      name = "grafana",
-      repo = "https://grafana.github.io/helm-charts",
-      version = "9.3.1",
+      name      = "grafana",
+      repo      = "https://grafana.github.io/helm-charts",
+      version   = "9.3.1",
+      namespace = "homelab"
+    },
+    {
+      name      = "opentelemetry-collector",
+      repo      = "https://open-telemetry.github.io/opentelemetry-helm-charts",
+      version   = "0.48.0",
       namespace = "homelab"
     },
   ]
