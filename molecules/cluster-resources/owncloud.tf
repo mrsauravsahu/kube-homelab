e21a542782b8427ef375a1be47fb4326495b5a7a
Saurav Sahu <mrsauravsahu@outlook.com>
2023-10-07 18:50:57 +0530

    improv: update and use ext4 external harddrive to store data

--- molecules/cluster-resources/owncloud.tf
+++ molecules/cluster-resources/owncloud.tf
@@ -1,9 +1,40 @@
+resource "helm_release" "pv_files" {
+  name       = "homelab-pv-files"
+  chart      = "./lib/helm-chart-openebs-pv"
+  namespace  = "homelab"
+
+  values = [
+    <<EOF
+    pv:
+      enabled: true
+      name: homelab-pv-files
+      storageClassName: openebs-hostpath
+      storage: 1.8Ti
+      path: /homelab/drives/drive-2tb-1/files
+      accessModes:
+        - ReadWriteMany
+      nodeAffinity:
+        required:
+          nodeSelectorTerms:
+          - matchExpressions:
+            - key: kubernetes.io/hostname
+              operator: In
+              values:
+              - pi
+    EOF
+  ]
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
 resource "helm_release" "owncloud" {
   name       = "owncloud"
   chart      = "./apps/owncloud"
   namespace  = "homelab"
   values = [file("./config/externals/owncloud/values.yaml")]
-  depends_on = [helm_release.homelab_data_pv]
+  depends_on = [helm_release.pv_files]
 }
 
 resource "helm_release" "owncloud_ingress" {
