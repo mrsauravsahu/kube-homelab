30e20cf6d59e8cf93139b5a18cf4e564bb7317db
Example <mrsauravsahu@outlook.com>
2024-05-28 22:57:34 +0530

    chore: create cnpg operator and database clusters

---
+++ molecules/cluster-resources/databases.tf
@@ -0,0 +1,20 @@
+resource "helm_release" "cnpg_operator" {
+  count = length(var.databases) > 0 ? 1 : 0
+
+  repository = "https://cloudnative-pg.github.io/charts"
+  name       = "cloudnative-pg"
+  chart      = "cloudnative-pg"
+  namespace  = "cnpg-system"
+  create_namespace = true
+  timeout = 600
+}
+
+resource "helm_release" "cnpg_clusters" {
+  for_each = { for db in var.databases : db.name => db if db.type == "postgresql" }
+  repository = "https://cloudnative-pg.github.io/charts"
+  name       = each.value.name
+  chart      = "cluster"
+  namespace  = each.value.namespace != null ? each.value.namespace : each.value.name
+  create_namespace = true
+  timeout = 600
+}
